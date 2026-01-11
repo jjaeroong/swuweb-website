3e3adfdbc334c03bde1ecaf478598f8834e5ff82
starday119 <jy8294@naver.com>
2026-01-12 01:03:02 +0900

    init:프로젝트 초기 구성

---
+++ src/main/kotlin/com/swuweb/swuweb/SwuwebApplication.kt
@@ -0,0 +1,11 @@
+package com.swuweb.swuweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwuwebApplication
+
+fun main(args: Array<String>) {
+    runApplication<SwuwebApplication>(*args)
+}
