202cdf45a1450c3ea249ce458f4bca9d77d63cc7
starday119 <jy8294@naver.com>
2026-01-14 07:30:27 +0900

    refactor : cors 에러 해결

---
+++ src/main/kotlin/com/swuweb/swuweb/WebCorsConfig.kt
@@ -0,0 +1,21 @@
+package com.swuweb.swuweb
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebCorsConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:5173",
+                "swuweb-homepage.vercel.app"
+            )
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(false)
+            .maxAge(3600)
+    }
+}
